8a49593b3520f08561666662c48fecf71ab7d8e4
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-12-05 08:54:44 -0500

    day1 part b

--- src/day1/day1.zig
+++ src/day1/day1.zig
@@ -1,10 +1,6 @@
 const std = @import("std");
 
-const Day1Error = error{
-    MissingNum, // line does not have number on it
-};
-
-pub fn sol() !u32 {
+pub fn solA() !u32 {
     // TODO assume we are running from project root
     const file = try std.fs.cwd().openFile("src/day1/input.txt", .{});
     defer file.close();
@@ -40,5 +36,69 @@ pub fn sol() !u32 {
 
         tot += front_char.? * 10 + back_char.?;
     }
+    return tot;
+}
+
+pub fn solB() !u32 {
+    const file = try std.fs.cwd().openFile("src/day1/input.txt", .{});
+    defer file.close();
+
+    const Mapping = struct { str: []const u8, num: u32 };
+
+    const mapping_table = [_]Mapping{
+        Mapping{ .str = "0", .num = 0 },
+        Mapping{ .str = "1", .num = 1 },
+        Mapping{ .str = "2", .num = 2 },
+        Mapping{ .str = "3", .num = 3 },
+        Mapping{ .str = "4", .num = 4 },
+        Mapping{ .str = "5", .num = 5 },
+        Mapping{ .str = "6", .num = 6 },
+        Mapping{ .str = "7", .num = 7 },
+        Mapping{ .str = "8", .num = 8 },
+        Mapping{ .str = "9", .num = 9 },
+        Mapping{ .str = "one", .num = 1 },
+        Mapping{ .str = "two", .num = 2 },
+        Mapping{ .str = "three", .num = 3 },
+        Mapping{ .str = "four", .num = 4 },
+        Mapping{ .str = "five", .num = 5 },
+        Mapping{ .str = "six", .num = 6 },
+        Mapping{ .str = "seven", .num = 7 },
+        Mapping{ .str = "eight", .num = 8 },
+        Mapping{ .str = "nine", .num = 9 },
+    };
+
+    var tot: u32 = 0;
+    while (true) {
+        // TODO assume no utf8
+        var buffer: [100]u8 = undefined;
+        const line = (try file.reader().readUntilDelimiterOrEof(&buffer, '\n')) orelse break;
+        const line_len = line.len;
+
+        var val: u32 = 0;
+
+        var i: usize = 0;
+        left_outer: while (i < line_len) : (i += 1) {
+            for (mapping_table) |mapping| {
+                if (std.mem.eql(u8, line[i..@min(i + mapping.str.len, line_len)], mapping.str)) {
+                    // std.debug.print("found {s}\n", .{mapping.str});
+                    val += 10 * mapping.num;
+                    break :left_outer;
+                }
+            }
+        }
+
+        i = 0;
+        right_outer: while (i < line_len) : (i += 1) {
+            for (mapping_table) |mapping| {
+                if (std.mem.eql(u8, line[line_len -| i -| mapping.str.len .. line_len - i], mapping.str)) {
+                    // std.debug.print("found {s}\n", .{mapping.str});
+                    val += mapping.num;
+                    break :right_outer;
+                }
+            }
+        }
+        tot += val;
+    }
+
     return tot;
 }
