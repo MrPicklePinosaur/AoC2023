8f48f029aae7626bf9c918e490af7c535a8851e0
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-12-11 04:46:35 -0500

    wip day4 part 2

--- src/day4/day4.zig
+++ src/day4/day4.zig
@@ -11,6 +11,7 @@ pub fn solA() !u32 {
     const allocator = arena.allocator();
 
     var winning_nums = std.AutoHashMap(u32, void).init(allocator);
+    defer winning_nums.deinit();
 
     var tot: u32 = 0;
     while (true) {
@@ -40,5 +41,65 @@ pub fn solA() !u32 {
             tot += std.math.pow(u32, 2, matches -| 1);
         }
     }
+    return tot;
+}
+
+pub fn solB() !u32 {
+    const file = try std.fs.cwd().openFile("src/day4/input.txt", .{});
+    defer file.close();
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    var winning_nums = std.AutoHashMap(u32, void).init(allocator);
+    defer winning_nums.deinit(); // TODO technically not necessary (since we use arena allocator)
+
+    // TODO not best choice of data structure?
+    var games = std.ArrayList(std.ArrayList(u32)).init(allocator);
+
+    var tot: u32 = 0;
+    while (true) {
+        var buffer: [256]u8 = undefined;
+        const line = (try file.reader().readUntilDelimiterOrEof(&buffer, '\n')) orelse break;
+
+        winning_nums.clearAndFree();
+
+        const trimmed = line[mem.indexOf(u8, line, ":").? + 2 ..];
+        var card_sections = mem.split(u8, trimmed, "|");
+
+        var my_nums_it = mem.tokenize(u8, card_sections.next().?, " ");
+        var winning_nums_it = mem.tokenize(u8, card_sections.next().?, " ");
+
+        while (winning_nums_it.next()) |num| {
+            try winning_nums.put(try std.fmt.parseInt(u32, num, 10), {});
+        }
+
+        var matched_nums = std.ArrayList(u32).init(allocator);
+
+        while (my_nums_it.next()) |num| {
+            const num_parsed = try std.fmt.parseInt(u32, num, 10);
+            if (winning_nums.get(num_parsed) != null) {
+                try matched_nums.append(num_parsed);
+            }
+        }
+
+        try games.append(matched_nums);
+    }
+
+    const num_games = games.items.len;
+    var table: []u32 = try allocator.alloc(u32, num_games);
+    @memset(table, 1);
+
+    // TODO i think need to do topological sort first
+
+    var i: usize = 0;
+    while (i < num_games) : (i += 1) {
+        print("===\n", .{});
+        for (games.items[i].items) |match| {
+            print("{d}\n", .{match});
+        }
+    }
+
     return tot;
 }
--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,6 @@ const day3 = @import("day3/day3.zig");
 const day4 = @import("day4/day4.zig");
 
 pub fn main() !void {
-    std.debug.print("day4 solution A {d}\n", .{try day4.solA()});
-    // std.debug.print("day4 solution B {d}\n", .{try day4.solB()});
+    // std.debug.print("day4 solution A {d}\n", .{try day4.solA()});
+    std.debug.print("day4 solution B {d}\n", .{try day4.solB()});
 }
