9978d1b959b7714756bd18935287f63946ca5aaa
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-12-17 01:44:05 -0500

    init go project

---
+++ go/aoc.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/MrPicklePinosaur/AoC2023/pkg"
+)
+
+func main() {
+	fmt.Println("hello world")
+	day5.SolA();
+
+}
---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module github.com/MrPicklePinosaur/AoC2023
+
+go 1.18
